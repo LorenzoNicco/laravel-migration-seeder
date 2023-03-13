6c2da4ed644b4603ec704d84998d2c823e8d6e74
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-13 18:26:43 +0100

    Add seeder con faker/ms3

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Train;
+
+// helpers
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i<10; $i++) {
+            $train = new Train();
+            $train->azienda = 'Trenitalia';
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->codice_treno = $faker->randomNumber(6, true);
+            $train->numero_carrozze = $faker->randomNumber(2,true);
+            $train->puntuale = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            $train->data = $faker->date();
+            $train->save();
+        }
+
+    }
+}
