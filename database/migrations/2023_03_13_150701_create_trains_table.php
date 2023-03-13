d0bae7230a60f65f4fb4c37dd4364cbd5cb73fb6
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-13 16:08:20 +0100

    Add tabella e colonne tramite artisan create

---
+++ database/migrations/2023_03_13_150701_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('azienda', 50);
+            $table->string('stazione_partenza', 50);
+            $table->string('stazione_arrivo', 50);
+            $table->unsignedInteger('codice_treno');
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->boolean('puntuale');
+            $table->boolean('cancellato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
